fab08a15f53b1726e62c4177e2c75fcfc55dfd4c
lvbealr <lvbealr@mail.ru>
2025-04-16 00:41:38 +0300

    update testing text

--- txt/main.py
+++ txt/main.py
@@ -1,11 +1,12 @@
 import re
 
-with open("lesMiserables.txt", "r", encoding="utf-8") as f:
+with open("text.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
 words = re.findall(r"\b[a-zA-Z']+\b", text)
 words = [word.lower() for word in words if 5 <= len(word) <= 32]
 words = [word for word in words if word.strip()]
+words = [word for word in words if words.count(word) == 1]
 
 with open("text.txt", "w", encoding="utf-8") as f:
     f.write("\n".join(words))
